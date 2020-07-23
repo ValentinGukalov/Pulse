f471cef35943d928bd53e156eac6c2ea900296a8
ValentinGukalov <valentingukalov@gmail.com>
2020-07-23 13:46:27 +0500

    +validated forms +phone mask

--- src/js/script.js
+++ src/js/script.js
@@ -52,6 +52,37 @@ $(document).ready(function() {
         })
     })
 
+    function validateForms(form) {
+        $(form).validate({
+            rules: {
+                name: {
+                    required: true,
+                    minlength: 2
+                    },
+                phone: "required",
+                email: {
+                    required: true,
+                    email: true
+                }
+            },
+            messages: {
+                name: {
+                    required: "Пожалуйста, введите своё имя",
+                    minlength: jQuery.validator.format("Как минимум {0} символа требуется ввести")
+                },
+                phone: "Пожалуйста, введите свой мобильный телефон",
+                email: {
+                    required: "Пожалуйста, введите свою почту",
+                    email: "Ваша почта должна быть следующего формата example@domain.com"
+                }
+                }
+        });
+    };
+    validateForms('#consultation-form');
+    validateForms('#consultation form');
+    validateForms('#order form');
+
+    $('input[name=phone]').mask("+7 (999) 999-99-99");
 });
 
 
